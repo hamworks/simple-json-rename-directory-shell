f781af919610c7cc6bda1effcc1694a0339e2198
Takenori Okashita (GOUTEN) <gouten@dotlinesquare.com>
2020-12-25 23:31:51 +0900

    Upload sh file.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# simple-json-rename-directory-shell
+# Simple JSON Rename Directory Shell
 Shell Script for rename and move directories using JSON files.
+
+## Prerequisites
+You need there.
+
+* jq
+
+### jq
+https://stedolan.github.io/jq/
+
+#### Install
+Homebrew
+```
+$ brew install jq
+```
+
+## How to use
+1. Open `rename.sh` file.
+2. Rewrite JSON name, source directory name, and destination directory name.
+3. Save `rename.sh` file.
+4. Open terminal app, execute next command.
+
+```
+$ sh rename.sh
+```
---
+++ rename.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+json=$(cat rename.json)
+srcDir=src
+destDir=dist
+
+mkdir $destDir
+cd $srcDir
+
+length=$(echo $json | jq length)
+
+echo 'Processed to...'
+for i in $( seq 0 $(($length - 1)) );
+do
+	dir=$(echo $json | jq -r .[$i].dir_id)
+	change=$(echo $json | jq -r .[$i].change_name)
+	if [ -e $dir ]; then
+		mv $dir $change
+		mv $change -t ../$destDir
+		echo '😁 No.'$i' SUCCESS: Change dir name to '\"${change}\"' from '\"${dir}\"'.'
+	else
+	    echo '❌ No.'$i' FAILED: Cannot find dir '\"${dir}\"'.'
+	fi
+done
+
+echo 'Completed! 🍺 Well done!'
